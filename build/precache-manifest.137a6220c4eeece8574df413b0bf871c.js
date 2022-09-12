ca9dfc501cefafebbe006585a71ccca75db2c8e5
Jesus Antonio Garza <jesus.garza@banregio.com>
2022-09-12 10:09:24 -0500

    front

---
+++ build/precache-manifest.137a6220c4eeece8574df413b0bf871c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2a81268b7aaf6a9d55fc21335e7ec7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "da0e24922760cc271edc",
+    "url": "./static/css/2.34d5dea0.chunk.css"
+  },
+  {
+    "revision": "6ea64fc8e225f7d08f1a",
+    "url": "./static/css/main.0f9b5c94.chunk.css"
+  },
+  {
+    "revision": "da0e24922760cc271edc",
+    "url": "./static/js/2.78089aad.chunk.js"
+  },
+  {
+    "revision": "8a7566b8da052bdc053aa18f973823a2",
+    "url": "./static/js/2.78089aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea64fc8e225f7d08f1a",
+    "url": "./static/js/main.fa04b931.chunk.js"
+  },
+  {
+    "revision": "a4c8566387921360dbad",
+    "url": "./static/js/runtime-main.c99a75af.js"
+  },
+  {
+    "revision": "73e6b012bf1e5ab3ba41033aa8f977de",
+    "url": "./static/media/flags_responsive.73e6b012.png"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "./static/media/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "./static/media/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "./static/media/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "./static/media/primeicons.e01fd413.eot"
+  }
+]);
